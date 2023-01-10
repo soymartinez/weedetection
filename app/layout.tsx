b4bb1ed9815a9ff700b072256dfa074efe1153b5
omgismartinez <martnzomg@gmail.com>
2023-01-09 19:35:36 -0600

    🌌 refactor (layout): local font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css'
+import localFont from '@next/font/local'
+
+const cocoGothicFat = localFont({ src: './coco-gothic-fat.woff2' })
 
 export default function RootLayout({
   children,
@@ -6,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cocoGothicFat.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
