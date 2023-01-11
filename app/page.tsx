4790061f27e93ddb5d4382244e9d0ab1cac59b37
omgismartinez <martnzomg@gmail.com>
2023-01-10 19:53:08 -0600

    🌌 refactor (page): landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,90 +1,33 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client"
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image'
+import { Camera } from 'components/icons'
+import { useState } from 'react'
 
 export default function Home() {
+  const [modal, setModal] = useState(false)
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className={`bg-black/10`}>
+      <div className={`h-screenn`}>
+        <section className={`bg-primary flex flex-col items-center overflow-hidden text-center py-5 px-2 rounded-b-3xl`}>
+          <Image
+            src={`/logo.png`}
+            alt={`Weedetection`}
+            width={120}
+            height={120}
+          />
+          <h1 className='text-4xl font-black mt-2'>Weedetection</h1>
+          <h3 className='text-2xl font-black mt-3'>¿Qué cepa deseas escanear?</h3>
+          <input className='outline-primary w-10/12 h-12 mt-3 px-4 rounded-2xl' type='text' />
+          <div className='bg-white h-1 w-2/5 mt-3 rounded-full' />
+        </section>
+        <section className='flex flex-col items-center justify-center min-h-[365px]'>
+          <label onClick={() => setModal(!modal)}>
+            <Camera className='text-white w-28 md:w-48 cursor-pointer' />
+          </label>
+          <input hidden type={'file'} accept={'.png, .jpg, .jpeg'} id='upload' />
+          <h3 className='text-primary text-3xl font-black mt-8'>Toma una foto</h3>
+        </section>
       </div>
     </main>
   )
