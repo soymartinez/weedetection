858e844fb6d6626e1090baba9291f8391070e4c2
omgismartinez <martnzomg@gmail.com>
2023-01-12 21:16:10 -0600

    🌌 refactor: scan & scan-line animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
       colors: {
         primary: "#4cffa3",
       },
+      animation: {
+        "scan": "scan 4s ease-in-out infinite",
+        "scan-line": "scan-line 4s ease-in-out infinite",
+      },
+      keyframes: {
+        "scan": {
+          "0%, 100%": { height: "0%" },
+          "50%": { height: "100%" },
+        },
+        "scan-line": {
+          "0%, 100%": { top: "0%" },
+          "50%": { top: "100%" },
+        }
+      }
     },
   },
   plugins: [],
